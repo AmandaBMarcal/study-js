c00e18df9c9238acf42109b704b2c565f63575f4
Amanda Marcal <amandabmarcal@gmail.com>
2021-03-04 15:23:14 -0500

    this is me re doing the method practice for practice

---
+++ methodPractice2.js
@@ -0,0 +1,10 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order.
+
+const multipliedValues = arrayInt => {
+
+}
+
+console.log(multipliedValues([1, 2, 3, 4])); //
+console.log(multipliedValues([1, 3, 3, 4])); //
+console.log(multipliedValues([1, 2, 0, 44])); //
+console.log(multipliedValues([3, 4])); //
